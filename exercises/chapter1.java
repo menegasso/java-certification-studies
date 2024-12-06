b1eebd47e99eff5c7e6063728c36d33840b3b01c
Andre Menegasso <meneghasso@gmail.com>
2024-12-06 10:56:07 -0300

    Atividades capítulo 1.1

---
+++ exercises/chapter1.java
@@ -0,0 +1,24 @@
+public class chapter1{
+
+	public static void main(String[] args){
+		System.out.println("Teste de comentários");
+		/*
+ 		* // anteater
+ 		*/
+
+		//bear
+
+		// //cat
+
+		// /* dog */
+
+		/* elephant */
+
+		/*
+ 		* /* ferret */
+ 		
+
+		
+
+	}
+}
